7c2e0a13983757217de13cd0553f178eee9a01fb
masscreed <36214853+masscreed@users.noreply.github.com>
2018-06-02 23:48:10 +0700

    Fix warning

--- src/nim.h
+++ src/nim.h
@@ -6,7 +6,7 @@ void mass_output(int mass[3]);
 int heap_selection();
 int heap_check(int input, int mass[3], int f);
 int take_number_matches();
-int* heap_change(int* mass, int i, int num);
+void heap_change(int* mass, int i, int num);
 int mechanics_of_the_game (int mass[3]);
 void start_game_nim();
 int end_nim (name *names_players, score *score, int status);
