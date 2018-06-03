87261fe2db841519af611a221271e9f961c54cbd
winterdrago <dbynthfynjy@gmail.com>
2018-06-03 23:24:42 +0700

    Add tests

--- test/test.c
+++ test/test.c
@@ -25,10 +25,32 @@ CTEST (mathces_test_false, check_correct_matches)
 }
 
 
+CTEST (mathces_test_false_1, check_correct_matches)
+{
+    int value[] = { 23, 5, 7};
+	int i = 0, num = -6;
+    int result = check_correct_matches (value, i, num);
+
+    const int expected = 0;
+    ASSERT_EQUAL (expected, result);
+}
+
+
 CTEST (heap_test_false, heap_check)
 {
     int value[] = { 23, 5, 7};
 	int input = 0;
+    int result = heap_check (input, value);
+
+    const int expected = 1;
+    ASSERT_EQUAL (expected, result);
+}
+
+
+CTEST (heap_test_false_1, heap_check)
+{
+    int value[] = { 23, 5, 7};
+	int input = -3;
     int result = heap_check (input, value);
 
     const int expected = 1;
@@ -85,6 +107,21 @@ CTEST (tick_test_false_1, check_correct_data)
         " | | "		
 	};
 	char a[] = "11";
+    int result = check_correct_data (a, ch);
+
+    const int expected = 0;
+    ASSERT_EQUAL (expected, result);
+}
+
+
+CTEST (tick_test_false_2, check_correct_data)
+{	
+	char ch[const_height][const_length] = {
+        "x| | ",
+        " | | ",
+        " | | "		
+	};
+	char a[] = "00";
     int result = check_correct_data (a, ch);
 
     const int expected = 0;
